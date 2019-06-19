23308e7002182da21e031fb27fe3de3090a73e47
johannes <jmuljana@gmail.com>
2019-06-19 14:14:05 +1000

    chart working, but data not sorted

--- src/app/stock-detail/stock-detail.page.ts
+++ src/app/stock-detail/stock-detail.page.ts
@@ -31,27 +31,51 @@ export class StockDetailPage implements OnInit {
     .then( (response:Observable<PriceData[]>) => {
       this.priceData$ = response;
       this.priceData$.subscribe( (values) => {
-        let chartLabels:Array<Date> = [];
+        let chartLabels:Array<any> = [];
         let chartData:Array<number> = [];
         values.forEach( (value, index ) => {
-          chartLabels.push(index);
+          let date = value.time.toDate();
+          chartLabels.push( index );
           chartData.push(value.close);
         });
         this.chart = new Chart(this.chartCanvas.nativeElement,{
           type: 'line',
-          data: {
-            labels: chartLabels,
-            dataSets:[{
-              data: chartData,
-              label: 'Open',
-              borderColor: "red",
-              fill: false
-            }]
-          }
+            data: {
+                labels: chartLabels,
+                datasets: [{
+                    label: `${this.stock.symbol} prices`,
+                    data: chartData,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
         });
       });
     });
-    
     
   }
   close(){
